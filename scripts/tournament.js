ba211caa79a214ce7c1f74b107aa16bba02e2d7d
Michelangelo <michelangelo.ph@gmail.com>
2018-03-06 20:26:48 -0800

    Added jquery, got forms on contact and tournament page to display correctly

---
+++ scripts/tournament.js
@@ -0,0 +1,7 @@
+$( document ).ready(function() {
+  //console.log("hello");
+	$( "#divRankings" ).hide();
+	$( "#mmodes" ).hide();
+	$( "#ContentPlaceHolder1_hMathModes" ).hide();
+	$( "#ContentPlaceHolder1_hSchoolName" ).hide();
+});
