78569bfe7ea7c5727d84a8289390c32489b32949
Michelangelo <michelangelo.ph@gmail.com>
2018-03-08 20:29:18 -0800

    Cleaned batter sign in and added script

---
+++ scripts/battersignin.js
@@ -0,0 +1,8 @@
+$( document ).ready(function() {
+  //console.log("hello");
+	//$( "#ContentPlaceHolder1_lblSubscriptionError" ).hide();
+});
+
+function signIn(){
+	$( "#lblSignInMessage" ).text('Login Failed. Please try again.');
+}
