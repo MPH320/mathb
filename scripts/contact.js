a254a619192f3a5e9d6ae456963486761996542b
Michelangelo <michelangelo.ph@gmail.com>
2018-04-12 19:27:18 -0700

    contact us form is working

--- scripts/contact.js
+++ scripts/contact.js
@@ -1,18 +1,25 @@
 $( document ).ready(function() {
-  //console.log("hello");
-	//$( "#ContentPlaceHolder1_lblSubscriptionError" ).hide();
+
+	
+	
 });
 
-function submitContact(){
+function submitContact(event){
+	event.preventDefault();
+
 		$( "#ContentPlaceHolder1_lblEmailError" ).css('color', '#00ACDC');
 	if(!$("#ContentPlaceHolder1_txtEmailMessage").val()){
 		$( "#ContentPlaceHolder1_lblEmailError" ).text('Please enter a message.');
 	}else if(!$("#ContentPlaceHolder1_txtEmailAddress").val()){
-		$( "#ContentPlaceHolder1_lblEmailError" ).text('Please enter an email address.');			 
+		$( "#ContentPlaceHolder1_lblEmailError" ).text('Please enter an email address.');
   }else{
-		$( "#ContentPlaceHolder1_lblEmailError" ).css('color', '#48473E');
-		$( "#ContentPlaceHolder1_lblEmailError" ).text('Thank you for contacting us.');
-	
+
+		$.post("https://jumprock.co/mail/michelangelopereira", $.param($('.contactForm').serializeArray()), function(data) {
+			 $( "#ContentPlaceHolder1_lblEmailError" ).css('color', '#48473E');
+				$( "#ContentPlaceHolder1_lblEmailError" ).text('Thank you for contacting us.');
+    });
+		
+
 	}
 }
 
